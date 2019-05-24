edb576724f3ca0867911c035c37c589f7efc93f5
JohnDHopkins3 <50149593+JohnDHopkins3@users.noreply.github.com>
2019-05-23 20:00:40 -0700

    stuff

---
+++ 2D game/Assets/FallowMouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallowMouse : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    public float moveSpeed;
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = transform.position.z;
+        transform.position = Vector3.MoveTowards(transform.position, mousePosition, moveSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
