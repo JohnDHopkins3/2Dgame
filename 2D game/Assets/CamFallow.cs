61e3d456aec23e0ae8c83a2b091603a50c7612e4
JOHNDHOPKINS <45177624+JOHNDHOPKINS@users.noreply.github.com>
2019-05-24 13:54:35 -0400

    ragdoll

---
+++ 2D game/Assets/CamFallow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFallow : MonoBehaviour
+{
+    public Transform player;
+    public int movementSpeed = 100;
+    void Update()
+    {
+        Vector3 playerPosition = new Vector3(player.position.x, player.position.y, -5);
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, movementSpeed * Time.deltaTime);
+    }
+}
