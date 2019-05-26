b3c0e77ed3b9af029e34c37748e910733b40f0b0
JohnDHopkins3 <50149593+JohnDHopkins3@users.noreply.github.com>
2019-05-25 17:33:03 -0700

    stuff

---
+++ 2D game/Assets/TurnOff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOff : MonoBehaviour
+{
+    public float power=-50;
+    void Start()
+    {
+        GetComponent<PointEffector2D>().forceMagnitude=0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            GetComponent<PointEffector2D>().forceMagnitude = power;
+        }
+        else
+        {
+            GetComponent<PointEffector2D>().forceMagnitude = 0;
+        }
+    }
+}
